4e2228c630d93cc046852c39c1d5cb43f9c6258a
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2023-06-15 11:57:44 -0300

    Trabalhando com funções

---
+++ js/script-funcoes.js
@@ -0,0 +1,16 @@
+function logCurso() {
+  const nome = "JavaScript";
+  console.log(nome);
+}
+logCurso();
+
+function logCurso1(nome) {
+  console.log(nome);
+}
+logCurso1("HTML");
+
+function logCurso2(nome, horas) {
+  console.log(nome, horas, "horas");
+}
+logCurso2("CSS3", "36");
+
